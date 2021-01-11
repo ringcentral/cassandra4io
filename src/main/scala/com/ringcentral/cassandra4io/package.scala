fbbd5793892b7502d8096420bea6604cb4bd4d07
Ilya Vasin <ilya.vasin@ringcentral.com>
2021-01-11 19:29:58 +0300

    cr follow-up

--- src/main/scala/com/ringcentral/cassandra4io/package.scala
+++ src/main/scala/com/ringcentral/cassandra4io/package.scala
@@ -4,7 +4,7 @@ import java.util.Optional
 
 package object cassandra4io {
   // for cross-build between scala 2.13 and 2.12
-  implicit class RichOptional[A](o: Optional[A]) {
+  final implicit class RichOptional[A](private val o: Optional[A]) extends AnyVal {
     def asScala: Option[A] = if (o.isPresent) Some(o.get()) else None
   }
 }
