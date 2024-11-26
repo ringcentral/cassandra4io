0db82be8ec7d3a8a00593e3ccc07fe81f3c60f9a
Aleksandr Shabalin <shabalin.a@2gis.ru>
2024-11-26 17:02:51 +0300

    optimized hConsBindableValuesBuilder

--- src/main/scala/com/ringcentral/cassandra4io/cql/package.scala
+++ src/main/scala/com/ringcentral/cassandra4io/cql/package.scala
@@ -184,31 +184,9 @@ package object cql {
           override def binder: Binder[RT] = f.binder
         }
 
-      implicit def hConsBindableValuesBuilder[T: ColumnsValues, PT <: HList, RT <: HList](implicit
+      implicit def hConsBindableValuesBuilder[V[_] <: Values[_], T: ColumnsValues, PT <: HList, RT <: HList](implicit
         f: BindableBuilder.Aux[PT, RT]
-      ): BindableBuilder.Aux[Values[T] :: PT, T :: RT] = new BindableBuilder[Values[T] :: PT] {
-        override type Repr = T :: RT
-        override def binder: Binder[T :: RT] = {
-          implicit val hBinder: Binder[T]  = Values[T].binder
-          implicit val tBinder: Binder[RT] = f.binder
-          Binder[T :: RT]
-        }
-      }
-
-      implicit def hConsBindableRestrictionsBuilder[T: ColumnsValues, PT <: HList, RT <: HList](implicit
-        f: BindableBuilder.Aux[PT, RT]
-      ): BindableBuilder.Aux[KeyEqualsTo[T] :: PT, T :: RT] = new BindableBuilder[KeyEqualsTo[T] :: PT] {
-        override type Repr = T :: RT
-        override def binder: Binder[T :: RT] = {
-          implicit val hBinder: Binder[T]  = Values[T].binder
-          implicit val tBinder: Binder[RT] = f.binder
-          Binder[T :: RT]
-        }
-      }
-
-      implicit def hConsBindableAssignmentsBuilder[T: ColumnsValues, PT <: HList, RT <: HList](implicit
-        f: BindableBuilder.Aux[PT, RT]
-      ): BindableBuilder.Aux[Assignment[T] :: PT, T :: RT] = new BindableBuilder[Assignment[T] :: PT] {
+      ): BindableBuilder.Aux[V[T] :: PT, T :: RT] = new BindableBuilder[V[T] :: PT] {
         override type Repr = T :: RT
         override def binder: Binder[T :: RT] = {
           implicit val hBinder: Binder[T]  = Values[T].binder
